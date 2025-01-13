81983d36c4cf0b269aae93e1c0cd458caf7b93b9
FoxMoss <foxmoss@mediaology.com>
2025-01-13 00:13:13 -0600

    Metadata

--- src/main.tsx
+++ src/main.tsx
@@ -117,7 +117,7 @@ const HomePage: Component<{}, {}> = function () {
     `;
     let sponsors = css`
         display: flex;
-        [data-component="CardClickable"] {
+        & > span {
             margin: 10px;
         }
     `;
@@ -153,6 +153,7 @@ const GoogleCalander: Component<{}, {}> = function () {
                 height="600"
                 frameborder="0"
                 scrolling="no"
+                loading="lazy"
             ></iframe>
         </div>
     );
